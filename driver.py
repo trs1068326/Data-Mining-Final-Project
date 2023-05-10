12186ee45b809eefd07e13d0e2c0f620821e7450
Tyler Stott <64285751+trs1068326@users.noreply.github.com>
2023-05-09 21:42:37 -0400

    Add files via upload

---
+++ driver.py
@@ -0,0 +1,204 @@
+import os
+import torch
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from scipy.stats import norm
+from collections import Counter
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings(action="ignore")
+
+# Preprocessing Libraries
+
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+
+# Machine Learning Libraries
+
+import sklearn
+import xgboost as xgb
+from sklearn import tree
+from sklearn.svm import SVC
+from sklearn.metrics import roc_curve
+from imblearn.pipeline import Pipeline
+#from catboost import CatBoostClassifier
+from imblearn.over_sampling import SMOTE
+from sklearn.ensemble import StackingClassifier
+from sklearn.metrics import classification_report
+#from mlxtend.classifier import StackingCVClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import KFold, cross_validate
+from sklearn.metrics import recall_score, f1_score, roc_auc_score
+from imblearn.pipeline import make_pipeline as imbalanced_make_pipeline
+from sklearn.metrics import precision_recall_curve, average_precision_score
+from sklearn.model_selection import RepeatedStratifiedKFold, StratifiedKFold
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score
+
+
+
+
+
+# IMPORTING DATA
+
+bank_data = pd.read_csv("./data.csv")
+print(bank_data.head(5))
+print()
+print(bank_data.info())
+labels = bank_data['Bankrupt?']
+print(labels)
+new_data = bank_data.drop(['Bankrupt?'], axis = 1)
+
+def log_trans(data):
+    
+    for col in data:
+        skew = data[col].skew()
+        if skew > 0.5 or skew < -0.5:
+            data[col] = np.log1p(data[col])
+        else:
+            continue
+            
+    return data
+
+data_norm = log_trans(new_data)
+
+X_raw,X_test,y_raw,y_test  = train_test_split(data_norm, labels, test_size=0.1, stratify = labels, random_state = 42)
+
+
+
+# Stratified Cross Validation Splitting
+
+sss = StratifiedKFold(n_splits=5, shuffle=False)
+
+for train_index, test_index in sss.split(X_raw,y_raw):
+    
+    print("Train:", train_index, "Test:", test_index)
+    X_train_sm, X_val_sm = X_raw.iloc[train_index], X_raw.iloc[test_index]
+    y_train_sm, y_val_sm = y_raw.iloc[train_index], y_raw.iloc[test_index]
+
+# Check the Distribution of the labels
+
+
+# Turn into an array
+X_train_sm = X_train_sm.values
+X_val_sm = X_val_sm.values
+y_train_sm = y_train_sm.values
+y_val_sm = y_val_sm.values
+
+# See if both the train and test label distribution are similarly distributed
+train_unique_label, train_counts_label = np.unique(y_train_sm, return_counts=True)
+test_unique_label, test_counts_label = np.unique(y_val_sm, return_counts=True)
+print('-' * 84)
+
+print('Label Distributions: \n')
+print(train_counts_label/ len(y_train_sm))
+print(test_counts_label/ len(y_val_sm))
+
+
+
+
+# LOGISTIC REGRESSION
+
+# List to append the score and then find the average
+
+accuracy_lst_reg = []
+precision_lst_reg = []
+recall_lst_reg = []
+f1_lst_reg = []
+auc_lst_reg = []
+
+log_reg_sm = LogisticRegression()
+#log_reg_params = {}
+log_reg_params = {"penalty": ['l2'],
+                  'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
+                  'class_weight': ['balanced',None],
+                  'solver':['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']}
+
+rand_log_reg = RandomizedSearchCV(LogisticRegression(), log_reg_params, n_iter=4)
+
+
+for train, val in sss.split(X_train_sm, y_train_sm):
+    pipeline_reg = imbalanced_make_pipeline(SMOTE(sampling_strategy='minority'), rand_log_reg) 
+    # SMOTE happens during Cross Validation not before..
+    model_reg = pipeline_reg.fit(X_train_sm[train], y_train_sm[train])
+    best_est_reg = rand_log_reg.best_estimator_
+    prediction_reg = best_est_reg.predict(X_train_sm[val])
+    
+    accuracy_lst_reg.append(pipeline_reg.score(X_train_sm[val], y_train_sm[val]))
+    precision_lst_reg.append(precision_score(y_train_sm[val], prediction_reg))
+    recall_lst_reg.append(recall_score(y_train_sm[val], prediction_reg))
+    f1_lst_reg.append(f1_score(y_train_sm[val], prediction_reg))
+    auc_lst_reg.append(roc_auc_score(y_train_sm[val], prediction_reg))
+
+print('---' * 45)
+print('')
+print('Logistic Regression (SMOTE) results:')
+print('')
+print("accuracy: {}".format(np.mean(accuracy_lst_reg)))
+print("precision: {}".format(np.mean(precision_lst_reg)))
+print("recall: {}".format(np.mean(recall_lst_reg)))
+print("f1: {}".format(np.mean(f1_lst_reg)))
+print('')
+print('---' * 45)
+
+# Printing the classification report
+
+label = ['Fin.Stable', 'Fin.Unstable']
+pred_reg_sm = best_est_reg.predict(X_val_sm)
+print(classification_report(y_val_sm, pred_reg_sm, target_names=label))
+
+# END LOGISTIC REGRESSION
+
+
+
+# RANDOM FOREST CLASSIFIER
+
+# List to append the score and then find the average
+
+accuracy_lst_rfc = []
+precision_lst_rfc = []
+recall_lst_rfc = []
+f1_lst_rfc = []
+auc_lst_rfc = []
+
+rfc_sm = RandomForestClassifier()
+#rfc_params = {}
+rfc_params = {'max_features' : ['auto', 'sqrt', 'log2'],
+              'random_state' : [42],
+              'class_weight' : ['balanced','balanced_subsample'],
+              'criterion' : ['gini', 'entropy'],
+              'bootstrap' : [True,False]}
+    
+    
+rand_rfc = RandomizedSearchCV(rfc_sm, rfc_params, n_iter=4)
+
+
+for train, val in sss.split(X_train_sm, y_train_sm):
+    pipeline_rfc = imbalanced_make_pipeline(SMOTE(sampling_strategy='minority'), rand_rfc) # SMOTE happens during Cross Validation 
+    model_rfc = pipeline_rfc.fit(X_train_sm, y_train_sm)
+    best_est_rfc = rand_rfc.best_estimator_
+    prediction_rfc = best_est_rfc.predict(X_train_sm[val])
+    
+    accuracy_lst_rfc.append(pipeline_rfc.score(X_train_sm[val], y_train_sm[val]))
+    precision_lst_rfc.append(precision_score(y_train_sm[val], prediction_rfc))
+    recall_lst_rfc.append(recall_score(y_train_sm[val], prediction_rfc))
+    f1_lst_rfc.append(f1_score(y_train_sm[val], prediction_rfc))
+    auc_lst_rfc.append(roc_auc_score(y_train_sm[val], prediction_rfc))
+    
+print('---' * 45)
+print('')
+print("accuracy: {}".format(np.mean(accuracy_lst_rfc)))
+print("precision: {}".format(np.mean(precision_lst_rfc)))
+print("recall: {}".format(np.mean(recall_lst_rfc)))
+print("f1: {}".format(np.mean(f1_lst_rfc)))
+print('---' * 45)
+
+smote_prediction_rfc = best_est_rfc.predict(X_val_sm)
+print(classification_report(y_val_sm, smote_prediction_rfc, target_names=label))
+
+# END RFC
